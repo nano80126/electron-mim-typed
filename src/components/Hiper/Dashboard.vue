b4f91458b857dc9bc82a726777347e5597f7f5b8
Ke LiDuan <nano80126@gmail.com>
2021-01-17 22:30:28 +0800

    edit some style

--- src/components/Hiper/Dashboard.vue
+++ src/components/Hiper/Dashboard.vue
@@ -4,24 +4,23 @@
 		<v-row no-gutters align="stretch" class="mt-3" :style="{ height: `${$root.webHeight - 92}px` }">
 			<v-col class="d-flex flex-column align-start justify-between">
 				<v-card class="ml-3" flat outlined width="240px">
-					<v-card-subtitle class="text-center py-2 info darken-2">
+					<v-card-subtitle class="text-center py-2 grey" :class="isDarkMode ? 'darken-2' : 'lighten-2'">
 						<span class="font-weight-black subtitle-1">連線狀態</span>
 					</v-card-subtitle>
 					<v-divider />
-					<v-card-text class="py-1">
-						<v-row no-gutters align="center" justify="start">
-							<v-col cols class="font-weight-bold subtitle-2">伺服器</v-col>
-							<v-col cols class="text-center">
+					<v-card-text class="py-1 px-0">
+						<v-row no-gutters align="center" justify="start" class="">
+							<v-col cols class="ml-3 font-weight-bold subtitle-2">伺服器</v-col>
+							<v-col cols class="mr-3 text-center">
 								<v-chip :color="isWebsocket ? 'success' : 'error'" class="my-2">
 									{{ isWebsocket ? '連線' : '斷線' }}
 								</v-chip>
 							</v-col>
 
-							<div class="col-12" />
-							<!-- </v-row>
-						<v-row no-gutters align="center" justify="start"> -->
-							<v-col cols class="font-weight-bold subtitle-2">燒結爐</v-col>
-							<v-col cols class="text-center">
+							<v-divider class="col-12 grey" />
+
+							<v-col cols class="ml-3 font-weight-bold subtitle-2">燒結爐</v-col>
+							<v-col cols class="mr-3 text-center">
 								<v-chip :color="isWebsocket ? 'success' : 'error'" class="my-2">
 									{{ isConnected ? '連線' : '斷線' }}
 								</v-chip>
@@ -30,9 +29,9 @@
 					</v-card-text>
 				</v-card>
 
-				<div class="my-auto w-100" style="top: -20%;">
-					<v-sheet height="12px" color="grey darken-2" elevation="2" style="position: relative;">
-						<v-text-field
+				<div class="ml-3 my-auto" style="position: relative; top: -10%; width: calc(100% - 16px)">
+					<v-sheet height="8px" color="grey darken-2" elevation="2" style="position: relative;">
+						<!-- <v-text-field
 							v-model="flow"
 							label="流量"
 							class="flow-text"
@@ -42,15 +41,29 @@
 							:loading="flow != 0"
 							readonly
 							style="user-select: none;"
-						></v-text-field>
+						></v-text-field> -->
+						<v-chip class="mt-n3 flow-chip d-flex justify-center">
+							<span>{{ flow }}</span>
+							<v-icon class="ml-3" small :color="flow != 0 ? 'info' : 'grey'">fas fa-water</v-icon>
+						</v-chip>
 					</v-sheet>
 				</div>
 			</v-col>
 			<v-col cols="auto" class="d-flex flex-column">
-				<v-card class="mx-auto my-auto red" max-width="400px" style="border-radius: 40%;">
+				<v-card
+					class="mx-auto my-auto"
+					max-width="400px"
+					color="grey"
+					:class="isDarkMode ? 'darken-2' : 'lighten-3'"
+					style="border-radius: 40%;"
+				>
 					<v-card-text class="pa-5">
 						<v-row align="center" justify="start" class=" font-weight-bold subtitle-1">
-							<v-col cols="6" class="text-right deep-purple--text">
+							<v-col
+								cols="6"
+								class="text-right"
+								:class="isDarkMode ? 'lime--text text--lighten-2' : 'deep-purple--text text--darken-3'"
+							>
 								溫度設定值:
 							</v-col>
 							<v-col cols="4" class="d-flex justify-space-around">
@@ -58,7 +71,11 @@
 								<span>&deg;C</span>
 							</v-col>
 							<!--  -->
-							<v-col cols="6" class="text-right deep-purple--text">
+							<v-col
+								cols="6"
+								class="text-right"
+								:class="isDarkMode ? 'lime--text text--lighten-2' : 'deep-purple--text text--darken-3'"
+							>
 								上部溫度當前值:
 							</v-col>
 							<v-col cols="4" class="d-flex justify-space-around">
@@ -66,7 +83,11 @@
 								<span>&deg;C</span>
 							</v-col>
 							<!--  -->
-							<v-col cols="6" class="text-right deep-purple--text">
+							<v-col
+								cols="6"
+								class="text-right"
+								:class="isDarkMode ? 'lime--text text--lighten-2' : 'deep-purple--text text--darken-3'"
+							>
 								下部溫度當前值:
 							</v-col>
 							<v-col cols="4" class="d-flex justify-space-around">
@@ -74,7 +95,11 @@
 								<span>&deg;C</span>
 							</v-col>
 							<!--  -->
-							<v-col cols="6" class="text-right deep-purple--text">
+							<v-col
+								cols="6"
+								class="text-right"
+								:class="isDarkMode ? 'lime--text text--lighten-2' : 'deep-purple--text text--darken-3'"
+							>
 								爐內壓力值:
 							</v-col>
 							<v-col cols="4" class="d-flex justify-space-around">
@@ -82,7 +107,11 @@
 								<span>kPa</span>
 							</v-col>
 							<!--  -->
-							<v-col cols="6" class="text-right deep-purple--text">
+							<v-col
+								cols="6"
+								class="text-right"
+								:class="isDarkMode ? 'lime--text text--lighten-2' : 'deep-purple--text text--darken-3'"
+							>
 								爐內真空值:
 							</v-col>
 							<v-col cols="4" class="d-flex justify-space-around">
@@ -99,29 +128,41 @@
 			<div class="col-12" />
 
 			<v-col cols="4" class="mt-auto">
-				<v-card class="ml-3 rounded-lg grey" min-width="360" max-height="500" style="">
+				<v-card class="ml-3 rounded-lg" outlined min-width="360" max-height="500" style="">
 					<v-card-text>
-						<v-row align="start" justify="center">
-							<v-col cols="4" class="font-weight-bold subtitle-1 primary--text">當前步剩餘時間:</v-col>
-							<v-col cols="8" class="font-weight-bold subtitle-1 d-flex justify-space-around">
+						<v-row align="start" justify="center" class="subtitle-1">
+							<v-col cols="4" class="font-weight-bold  primary--text">
+								當前步剩餘時間:
+							</v-col>
+							<v-col cols="8" class="font-weight-bold text-center d-flex justify-space-around">
 								<span>{{ leaveTime }}</span>
 								<span class="ml-5">分鐘</span>
 							</v-col>
 							<!--  -->
-							<v-col cols="4" class="font-weight-bold subtitle-1 primary--text">當前工藝名稱:</v-col>
-							<v-col cols="8" class="font-weight-bold subtitle-1 text-center info--text">
+							<v-col cols="4" class="font-weight-bold primary--text">
+								當前工藝名稱:
+							</v-col>
+							<v-col cols="8" class="font-weight-bold text-center info--text">
 								<span>{{ workNowName || '(NULL)' }}</span>
 							</v-col>
 							<!--  -->
-							<v-col cols="4" class="font-weight-bold subtitle-1 primary--text">
+							<v-col cols="4" class="font-weight-bold primary--text">
 								當前工藝狀態:
 							</v-col>
 							<v-col
 								cols="8"
-								class="font-weight-bold subtitle-1 text-center"
+								class="font-weight-bold text-center"
 								:class="nowState && nowState <= 5 ? 'error--text' : 'info--text'"
 							>
 								<span>{{ workNowState || '(NULL)' }}</span>
+							</v-col>
+							<!--  -->
+							<v-col cols="4" class="font-weight-bold primary--text">
+								等待時間:
+							</v-col>
+							<v-col cols="8" class="font-weight-bold text-center d-flex justify-space-around">
+								<span>{{ waitTime }}</span>
+								<span>分鐘</span>
 							</v-col>
 						</v-row>
 					</v-card-text>
@@ -165,6 +206,10 @@ export default class HiperDashboard extends Vue {
 	get isWebsocket() {
 		return false;
 	}
+	/** */
+	get isDarkMode() {
+		return this.$vuetify.theme.dark;
+	}
 
 	mounted() {
 		//
@@ -177,20 +222,23 @@ export default class HiperDashboard extends Vue {
 
 	beforeDestroy() {
 		if (AppModule.isElectron) {
-			//
+			// remove serial event
+		} else {
+			// nothing to do
 		}
 	}
 }
 </script>
 
 <style lang="scss" scoped>
-.flow-text {
+.flow-chip {
 	position: absolute;
 	background-color: white;
-	left: 50%;
-	right: 0;
-	max-width: 80px;
-	margin: 0;
-	margin-top: -14px;
+	margin-left: 50%;
+	min-width: 80px;
+	// left: 0;
+	// right: 0;
+	// max-width: 80px;
+	// margin-top: -14px;
 }
 </style>
--- src/views/Hiper.vue
+++ src/views/Hiper.vue
@@ -3,39 +3,43 @@
 		<v-tabs v-model="tab" height="36" color="warning" background-color="green darken-3">
 			<v-tabs-slider color="transparent" />
 			<v-tab class="">
-				<span class="mr-2">面板</span>
+				<span class="mr-2 font-weight-bold">面板</span>
 				<v-icon small>fas fa-tachometer-alt</v-icon>
 			</v-tab>
 			<v-tab class="">
-				<span class="mr-2">爐溫曲線圖</span>
+				<span class="mr-2 font-weight-bold">爐溫曲線圖</span>
 				<v-icon small>fas fa-chart-line</v-icon>
 			</v-tab>
 			<v-spacer></v-spacer>
 
 			<v-tab class="">
-				<span class="mr-2">連線</span>
+				<span class="mr-2 font-weight-bold">連線</span>
 				<v-icon small>fas fa-network-wired</v-icon>
 			</v-tab>
 		</v-tabs>
 
-		<v-tabs-items v-model="tab">
+		<v-tabs-items v-model="tab" class="transparent">
 			<v-tab-item>
 				<Dashboard />
 			</v-tab-item>
 			<v-tab-item>456</v-tab-item>
-			<v-tab-item>連線</v-tab-item>
+			<v-tab-item>
+				<Connect />
+			</v-tab-item>
 		</v-tabs-items>
 	</div>
 </template>
 
 <script lang="ts">
 import board from '@/components/Hiper/Dashboard.vue';
+import connect from '@/components/Hiper/Connect.vue';
 
 import { Component, Vue } from 'vue-property-decorator';
 
 @Component({
 	components: {
-		Dashboard: board
+		Dashboard: board,
+		Connect: connect
 	}
 })
 export default class HIPER extends Vue {
